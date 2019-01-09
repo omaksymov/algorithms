f8f9c03932fe0ab985c6de9104ec2fee8a01b579
omaksymov <oleksandr.maksymov@gmail.com>
2019-01-09 19:10:49 +0200

    Added solution and tests for LeetCode "Projection Area  of 3D Shapes" problem (883)

---
+++ src/main/leetcode/ProjectionAreaOf3DShapes883.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+/**
+ * On a N * N grid, we place some 1 * 1 * 1 cubes that are axis-aligned with the x, y, and z axes.
+ * Each value v = grid[i][j] represents a tower of v cubes placed on top of grid cell (i, j).
+ *
+ * Now we view the projection of these cubes onto the xy, yz, and zx planes.
+ * A projection is like a shadow, that maps our 3 dimensional figure to a 2 dimensional plane.
+ * Here, we are viewing the "shadow" when looking at the cubes from the top, the front, and the side.
+ *
+ * Return the total area of all three projections.
+ *
+ * See examples in a test class.
+ *
+ * Note:
+ * 1 <= grid.length = grid[0].length <= 50
+ * 0 <= grid[i][j] <= 50
+ */
+public class ProjectionAreaOf3DShapes883 {
+    public int projectionArea(int[][] grid) {
+        int xy = countXYProjection(grid);
+        int xz = countXZProjection(grid);
+        int yz = countYZProjection(grid);
+        return xy + xz + yz;
+    }
+
+    // Every non-zero height of cell adds up into 1 area item
+    private int countXYProjection(int[][] a) {
+        int n = a.length;
+        int area = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] != 0) {
+                    area++;
+                }
+            }
+        }
+        return area;
+    }
+
+    // For every x find the maximum height z and add all max values - it will be xz projection area
+    private int countXZProjection(int[][] a) {
+        int n = a.length;
+        int area = 0;
+        for (int i = 0; i < n; i++) {
+            int max = a[i][0];
+            for (int j = 1; j < n; j++) {
+                if (a[i][j] > max) {
+                    max = a[i][j];
+                }
+            }
+            area += max;
+        }
+        return area;
+    }
+
+    // For every y find the maximum height z and add all max values - it will be yz projection area
+    private int countYZProjection(int[][] a) {
+        int n = a.length;
+        int area = 0;
+        for (int j = 0; j < n; j++) {
+            int max = a[0][j];
+            for (int i = 1; i < n; i++) {
+                if (a[i][j] > max) {
+                    max = a[i][j];
+                }
+            }
+            area += max;
+        }
+        return area;
+    }
+}
---
+++ src/test/leetcode/ProjectionAreaOf3DShapes883Test.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ProjectionAreaOf3DShapes883Test {
+    @Parameterized.Parameters
+    public static List<TestItem883> data() {
+        List<TestItem883> data = new ArrayList<>();
+        data.add(new TestItem883(new int[][]{{2}}, 5));
+        data.add(new TestItem883(new int[][]{{1, 2}, {3, 4}}, 17));
+        data.add(new TestItem883(new int[][]{{1, 0}, {0, 2}}, 8));
+        data.add(new TestItem883(new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}, 14));
+        data.add(new TestItem883(new int[][]{{2, 2, 2}, {2, 1, 2}, {2, 2, 2}}, 21));
+        return data;
+    }
+
+    private static class TestItem883 {
+        int[][] grid;
+        int expectedArea;
+
+        public TestItem883(int[][] grid, int expectedArea) {
+            this.grid = grid;
+            this.expectedArea = expectedArea;
+        }
+    }
+
+    private TestItem883 testItem;
+
+    public ProjectionAreaOf3DShapes883Test(TestItem883 testItem) {
+        this.testItem = testItem;
+    }
+
+    @Test
+    public void projectionArea() {
+        ProjectionAreaOf3DShapes883 solution = new ProjectionAreaOf3DShapes883();
+        int actualArea = solution.projectionArea(testItem.grid);
+        assertEquals(testItem.expectedArea, actualArea);
+    }
+}
